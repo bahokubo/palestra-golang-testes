d12dc86e32bbe08db1c8b638bdb9d7862863ab7c
bahcasac <barbara.casac@picpay.com>
2023-04-16 22:41:09 -0300

    adicionando novas funcoes no service

--- user/service.go
+++ user/service.go
@@ -1,5 +1,7 @@
 package user
 
+import "errors"
+
 var UserNotFound = "Error user not found"
 
 type Service struct {
@@ -20,28 +22,28 @@ func (s *Service) Create(users []*User) ([]*User, error) {
 	return users, nil
 }
 
-// func (s *Service) List() (users []*User, err error) {
-// 	if users, err = s.repo.List(); err != nil {
-// 		return nil, err
-// 	}
+func (s *Service) List() (users []*User, err error) {
+	if users, err = s.repo.List(); err != nil {
+		return nil, err
+	}
 
-// 	return users, nil
-// }
+	return users, nil
+}
 
-// func (s *Service) Update(user *User) (*User, error) {
-// 	if _, err := s.repo.Update(user); err != nil {
-// 		return nil, err
-// 	}
+func (s *Service) Update(user *User) (*User, error) {
+	if _, err := s.repo.Update(user); err != nil {
+		return nil, err
+	}
 
-// 	return user, nil
-// }
+	return user, nil
+}
 
-// func (s *Service) Delete(id string) error {
-// 	i, err := s.repo.Delete(id)
+func (s *Service) Delete(id string) error {
+	i, err := s.repo.Delete(id)
 
-// 	if i == 0 {
-// 		return errors.New(UserNotFound)
-// 	}
+	if i == 0 {
+		return errors.New(UserNotFound)
+	}
 
-// 	return err
-// }
+	return err
+}
